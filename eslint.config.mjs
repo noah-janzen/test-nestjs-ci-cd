5d1bc3227cacb888eb04dc7b73ed8b9c1982eec1
Noah Janzen <noah@noahjanzen.com>
2024-10-25 13:17:19 +0200

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,52 +1,38 @@
-import typescriptEslintEslintPlugin from '@typescript-eslint/eslint-plugin';
-import globals from 'globals';
-import tsParser from '@typescript-eslint/parser';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
+import { configs } from '@eslint/js';
+import { node, jest } from 'globals';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+import { configs as _configs, parser as _parser } from 'typescript-eslint';
+
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   {
-    ignores: ['**/.eslintrc.js'],
+    ignores: ['**/eslint.config.js'],
   },
-  ...compat.extends(
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ),
-  {
-    plugins: {
-      '@typescript-eslint': typescriptEslintEslintPlugin,
-    },
 
+  configs.recommended,
+  ..._configs.recommendedTypeChecked,
+  ..._configs.stylisticTypeChecked,
+  eslintConfigPrettier,
+
+  {
     languageOptions: {
       globals: {
-        ...globals.node,
-        ...globals.jest,
+        ...node,
+        ...jest,
       },
 
-      parser: tsParser,
-      ecmaVersion: 5,
-      sourceType: 'module',
-
+      parser: _parser,
       parserOptions: {
-        project: 'tsconfig.json',
-        tsconfigRootDir: '/Users/noahjanzen/dev/test-nestjs-ci-cd',
+        projectService: true,
+        tsconfigRootDir: __dirname,
       },
     },
-
+    // Rules
     rules: {
-      '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
     },
   },
