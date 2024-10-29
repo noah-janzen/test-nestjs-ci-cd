722090063817acee5d5d1998cbe885de9b44405e
Noah Janzen <noah@noahjanzen.com>
2024-10-29 12:56:15 +0100

    Revert "chore: update eslint config"
    This reverts commit 5d1bc3227cacb888eb04dc7b73ed8b9c1982eec1.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,52 @@
-import { configs } from '@eslint/js';
-import { node, jest } from 'globals';
+import typescriptEslintEslintPlugin from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
-import { configs as _configs, parser as _parser } from 'typescript-eslint';
-
-import eslintConfigPrettier from 'eslint-config-prettier';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
 
 export default [
   {
-    ignores: ['**/eslint.config.js'],
+    ignores: ['**/.eslintrc.js'],
   },
-
-  configs.recommended,
-  ..._configs.recommendedTypeChecked,
-  ..._configs.stylisticTypeChecked,
-  eslintConfigPrettier,
-
+  ...compat.extends(
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ),
   {
+    plugins: {
+      '@typescript-eslint': typescriptEslintEslintPlugin,
+    },
+
     languageOptions: {
       globals: {
-        ...node,
-        ...jest,
+        ...globals.node,
+        ...globals.jest,
       },
 
-      parser: _parser,
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'module',
+
       parserOptions: {
-        projectService: true,
-        tsconfigRootDir: __dirname,
+        project: 'tsconfig.json',
+        tsconfigRootDir: '/Users/noahjanzen/dev/test-nestjs-ci-cd',
       },
     },
-    // Rules
+
     rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
     },
   },
