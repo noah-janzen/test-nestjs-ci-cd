f02d1569c85d283a4706aec135bd68d4257d293d
Noah Janzen <noah@noahjanzen.com>
2024-11-23 09:21:31 +0100

    chore: add config module

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const PORT = configService.get('PORT');
+
+  await app.listen(PORT);
+  Logger.log(`Server is running port ${PORT}`, 'Bootstrap');
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
